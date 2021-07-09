c3814cee10d80c06238e3d7176f62b39ad270b7d
Ömer Emre Elmalı <omeremreelma@gmail.com>
2021-07-10 01:15:47 +0300

    end desktop design

--- js/swiper.js
+++ js/swiper.js
@@ -3,12 +3,12 @@ const swiper = new Swiper('.swiper-container', {
     breakpoints: {
       // when window width is >= 320px
       320: {
-        slidesPerView: 2.3,
+        slidesPerView: 2,
         spaceBetween: 5
       },
       // when window width is >= 480px
       480: {
-        slidesPerView: 3.3,
+        slidesPerView: 1.8,
         spaceBetween: 8
       },
       // when window width is >= 640px
