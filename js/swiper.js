1eef8b6bbed7e5e0db1485bfab7c770a4865fffe
Ömer Emre Elmalı <omeremreelma@gmail.com>
2021-07-10 00:03:49 +0300

    update design

---
+++ js/swiper.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper-container', {
+  
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2.3,
+        spaceBetween: 5
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3.3,
+        spaceBetween: 8
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 4.3,
+        spaceBetween: 17
+      }
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
