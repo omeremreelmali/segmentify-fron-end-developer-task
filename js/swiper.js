3ba811daa8861f759c1db459742d1c86a4505c00
Ömer Emre Elmalı <omeremreelma@gmail.com>
2021-07-10 03:15:48 +0300

    Update simple change

--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,14 @@
 const swiper = new Swiper('.swiper-container', {
   
     breakpoints: {
-      // when window width is >= 320px
       320: {
         slidesPerView: 2,
         spaceBetween: 5
       },
-      // when window width is >= 480px
       480: {
         slidesPerView: 1.8,
         spaceBetween: 8
       },
-      // when window width is >= 640px
       640: {
         slidesPerView: 3,
         spaceBetween: 17
@@ -21,8 +18,6 @@ const swiper = new Swiper('.swiper-container', {
         spaceBetween: 17
       }
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
